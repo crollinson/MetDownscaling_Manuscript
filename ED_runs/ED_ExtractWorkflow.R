313706fbcb99bec803c7d11d607647b94271ab93
Cori Butkiewicz <cori.butkiewicz@gmail.com>
2018-06-25 13:53:56 -0500

    Attempted for Loop

--- ED_runs/ED_ExtractWorkflow.R
+++ ED_runs/ED_ExtractWorkflow.R
@@ -2,13 +2,15 @@
 sitelat =  45.805822
 sitelon = -90.079722
 
-# You'll need to make the following into a loop where both ed.dir and out.dir get changed ot be separate folders for each ensemble
+# You'll need to make the following into a loop where both ed.dir and out.dir get changed to be separate folders for each ensemble
 # Some runs will have issues because of the start/end date in missing years (Ameriflux) and sims that didn't reach the end
+
 start_date = "2000-01-01"
 end_date   = "2014-12-31"
 
 ed.dir <- "4_runs/ed_runs.v1/NLDAS_raw/analy/"
 outdir <- "4_runs/extracted_output"
-if(!dir.exists(outdir)) dir.create(outdir)
+
+for (i in ed.dir){if(!dir.exists(outdir)) dir.create(outdir)}
 
 model2netcdf.ED2.MetDownscaling(ed.dir, outdir, sitelat, sitelon, start_date, end_date, pft_names = NULL, ed.freq="I")
