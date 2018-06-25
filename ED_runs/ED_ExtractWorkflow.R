1753e728aed43af29d310de4425f52293e881bf3
Christy Rollinson <crollinson@gmail.com>
2018-06-25 13:52:23 -0500

    Example script for workign with ED data

--- ED_runs/ED_ExtractWorkflow.R
+++ ED_runs/ED_ExtractWorkflow.R
@@ -7,8 +7,10 @@ sitelon = -90.079722
 start_date = "2000-01-01"
 end_date   = "2014-12-31"
 
-ed.dir <- "4_runs/ed_runs.v1/NLDAS_raw/analy/"
-outdir <- "4_runs/extracted_output"
+ed.dir <- "4_runs/ed_runs.v1/NLDAS_raw/analy/" # Where the raw data are
+outdir <- "4_runs/extracted_output" # Where we want to save our output
 if(!dir.exists(outdir)) dir.create(outdir)
 
+# Runs a function (that we need to source)
+source("model2netcdf.ed2.Downscaling.R")
 model2netcdf.ED2.MetDownscaling(ed.dir, outdir, sitelat, sitelon, start_date, end_date, pft_names = NULL, ed.freq="I")
